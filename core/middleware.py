79a126f0d3293eec93d941f9f544dfe684a5aff9
sjung <sebastian.jung@intense.de>
2024-07-19 10:58:48 +0200

    Move in settings.ini setting for base template

--- core/middleware.py
+++ core/middleware.py
@@ -2,4 +2,5 @@ from django.conf import settings
 def middleware(request):
     context = {}
     context['app_title'] = settings.APP_TITLE
+    context['base_template'] = settings.BASE_TEMPLATE
     return context
